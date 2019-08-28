be2f0e99fc9ae26c5b910774709e304118c04dfa
Lena Hamilton <hamiltonlena21@gmail.com>
2019-08-27 18:59:40 -0600

    Initialize project again

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Emi pics</title>
+</svelte:head>
+
+<h1>Collection of pictures of best cat ever.</h1>
+
+<div >
+	<img alt="Emi on a dresser" src="/Emi.jpg" width="700">
+	<br> <br>
+	<img alt="Emi having chin scratched" src="/pets.jpg" width="700" >
+	<br> <br>
+	<img alt="Emi hidding" src="/Under-dresser.jpg" width="700">
+	<br> <br>
+	<img alt="Emi standing on a human" src="/mt-butt.jpg"width="700" >
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,58 @@
+<style>
+  h1,
+  h3,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+    font-family: Georgia, "Times New Roman", Times, serif;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  h3 {
+    font-size: 1.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 5em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Emi pics (hopefuly)</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+<h3>You have found the pictures of the best cat!</h3>
+
+<figure>
+  <img alt="Emi" src="sittin-pretty.jpg" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<h3>
+  <strong>Go to Emi's page to see her pics.</strong>
+</h3>
